5ff28a0bf0da7b411b8952118716dd7e1713eb40
Corbin Bremmeyr <bremmeyc@mail.gvsu.edu>
2019-03-04 12:19:41 -0500

    Fixed changes with move getters/setters

--- src/Chess/Queen.java
+++ src/Chess/Queen.java
@@ -13,8 +13,8 @@ public class Queen extends ChessPiece {
 	}
 	
 	public boolean isValidMove(Move move, IChessPiece[][] board) {
-		Bishop move1 = new Bishop(board[move.fromRow][move.fromColumn].player());
-		Rook move2 = new Rook(board[move.fromRow][move.fromColumn].player());
+		Bishop move1 = new Bishop(board[move.getFromRow()][move.getFromColumn()].player());
+		Rook move2 = new Rook(board[move.getFromRow()][move.getFromColumn()].player());
 		return (move1.isValidMove(move, board) || move2.isValidMove(move, board));
 	}
 }
